96627b8884d7bfde0ff09b4ff264bc34aff3039a
ayushigupta0912 <aayushigupta0912@gmail.com>
2019-08-27 18:38:58 +0530

    #more updations

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Department } from '../models/department.model';
 
 @Component({
   selector: 'app-create-employee',
@@ -8,6 +9,13 @@ import { NgForm } from '@angular/forms';
 })
 export class CreateEmployeeComponent implements OnInit {
   // gender = 'female';  to set default value in radio-button
+  departments: Department[] = [
+    {id: 1 , name: 'IT'},
+    {id: 2 , name: 'HR'},
+    {id: 3 , name: 'Engg'},
+    {id: 4 , name: 'Payroll'},
+    {id: 5 , name: 'Admin'},
+  ];
   constructor() { }
 
   ngOnInit() {
